86f526194b035be7f16dc9f7185c51ee0801a905
Dmitriy <idmitriy.fsd@gmail.com>
2020-08-20 13:35:56 +0300

    Скорректированы стили (прозрачность и курсор) для отключенной карты

--- src/components/cat-card/cat-card.js
+++ src/components/cat-card/cat-card.js
@@ -72,28 +72,4 @@ $(document).ready(function () {
 
   });
 
-
-
-  $('.exp-checkbox').each(function (index, element) {
-    let $arrow = $(element).find('.exp-checkbox__arrow').html();
-    let isShown;
-
-    if ($arrow == 'expand_more') {
-      isShown = false;
-    } else if ($arrow == 'expand_less') {
-      isShown = true;
-    }
-
-    $(element).find(".exp-checkbox__title").click(function () {
-      if (isShown) {
-        $(element).find(".exp-checkbox__buttons").hide("slow");
-        $(element).find(".exp-checkbox__arrow").html('expand_more')
-      } else {
-        $(element).find(".exp-checkbox__buttons").show("slow");
-        $(element).find(".exp-checkbox__arrow").html('expand_less')
-      }
-      isShown = !isShown;
-    });
-
-  });
 });
